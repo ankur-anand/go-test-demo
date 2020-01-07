c0638f3f1f458613f96efa505a5f90d3b9f75de4
ankur-anand <ankur.anand12@gmail.com>
2020-01-07 14:54:06 +0530

    mocked example completed

--- unit-simple-service/unit-simple_test.go
+++ unit-simple-service/unit-simple_test.go
@@ -2,11 +2,25 @@ package unit_simple_service
 
 import "testing"
 
+// This helps in assigning mock at the runtime instead of compile time
+var userExistMock func(email string) bool
+
+type userDBMock struct{}
+
+func (u userDBMock) userExist(email string) bool {
+	return userExistMock(email)
+}
+
 func TestRegisterUser(t *testing.T) {
 	user := User{
 		Name:     "Ankur Anand",
 		Email:    "anand@example.com",
 		UserName: "anand",
+	}
+
+	userDatabase = userDBMock{}
+	userExistMock = func(email string) bool {
+		return false
 	}
 
 	err := RegisterUser(user)
